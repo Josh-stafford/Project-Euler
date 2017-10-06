5fa6eb20b2789bfa7973457e79c81fdb7fe903fb
Joshua Stafford <joshuastafford@Joshuas-MBP.home>
2017-10-06 15:54:12 +0100

    Completed task 10

---
+++ Summation_of_primes/main.py
@@ -0,0 +1,23 @@
+primeSum = 2
+num = 3
+count = 0
+
+while num < 2000000:
+    count+=1
+    isntPrime = False
+
+    for x in range(3, num/2):
+        if x % 2 != 0:
+            if(float(num) / float(x)) % 1 == 0:
+                isntPrime = True
+                break
+
+    if isntPrime == False:
+        primeSum += num
+
+    if count % 10000 == 0:
+        print(count)
+
+    num += 2
+
+print(primeSum)
